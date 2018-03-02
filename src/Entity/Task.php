2cbf20c3c4bf55d1149b1db07375468f3ef7e5cd
josefrnandezz <i62fealj@uco.es>
2018-03-02 19:00:00 +0100

    Dia 3

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
@@ -21,6 +22,8 @@ class Task
     /**
      * @var string
      * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="1", max="100")
      */
     private $description;
 
@@ -65,7 +68,7 @@ class Task
     /**
      * @return string
      */
-    public function getDescription(): string
+    public function getDescription()
     {
         return $this->description;
     }
@@ -74,7 +77,7 @@ class Task
      * @param string $description
      * @return Task
      */
-    public function setDescription(string $description): Task
+    public function setDescription($description)
     {
         $this->description = $description;
         return $this;
@@ -83,7 +86,7 @@ class Task
     /**
      * @return bool
      */
-    public function isDone(): bool
+    public function isDone()
     {
         return $this->isDone;
     }
@@ -92,7 +95,7 @@ class Task
      * @param bool $isDone
      * @return Task
      */
-    public function setIsDone(bool $isDone): Task
+    public function setIsDone($isDone)
     {
         $this->isDone = $isDone;
         return $this;
@@ -101,7 +104,7 @@ class Task
     /**
      * @return \DateTime
      */
-    public function getCreatedAt(): \DateTime
+    public function getCreatedAt()
     {
         return $this->createdAt;
     }
@@ -110,7 +113,7 @@ class Task
      * @param \DateTime $createdAt
      * @return Task
      */
-    public function setCreatedAt(\DateTime $createdAt): Task
+    public function setCreatedAt($createdAt)
     {
         $this->createdAt = $createdAt;
         return $this;
